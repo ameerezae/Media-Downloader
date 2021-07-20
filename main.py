39e2c82e8a0d925bd8a0e8ea907d20b9c934fab3
ameerezae <amir.rezaei1999@yahoo.com>
2021-07-20 14:27:41 +0430

    Add docker and docker compose file for downloader

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
-import argparse
 from prometheus_client import start_http_server, Gauge
 from downloader import Downloader
 from file_reader import csv_file_reader
+import os
 
-parser = argparse.ArgumentParser()
+def get_env(var_name, default_value='', required=False):
+    if type(var_name) != str or type(default_value) != str:
+        return ValueError("var_name and default_value must be string")
+    var_val = os.environ.get(var_name, default_value).strip()
+    if var_val == "" and required:
+        return ValueError(var_name + " not found in envirenment variables")
+    return var_val
 
-parser.add_argument("-m", "--mode", help="Set download mode")
-parser.add_argument("-t", "--threads", help="Set number of threads")
 
-args = parser.parse_args()
-if args.mode:
-    mode = str(args.mode)
-if args.threads:
-    number_of_threads = int(args.threads)
+def _get_downloader_config():
+    return {
+        "mode": get_env("DOWNLOADER_MODE", required=True),
+        "timeout": float(get_env("REQUEST_TIMEOUT", required=True)),
+        "number_of_th": int(get_env("DOWNLOADER_THREAD_NO", required=True)),
+        "downloading_path": get_env("DOWNLOADING_PATH", required=True),
+        "max_retry": int(get_env("DOWNLOADER_MAX_RETRY", required=True)),
+        "conn_establish_timeout": float(get_env("CONNECTION_ESTABLISHMENT_TIMEOUT", required=True)),
+    }
 
 
 if __name__ == '__main__':
 
     start_http_server(8000)
-    mode = "VPN"
-    number_of_threads = 25
-    if args.mode:
-        mode = str(args.mode)
-    if args.threads:
-        number_of_threads = int(args.threads)
-
     filename, images_index = ("new-instagram.csv", "urls")
     urls = csv_file_reader(filename, images_index)
     urls = [url for url in urls if url != '']
     batch_size = 100
+
     urls_length = len(urls)
 
-    print(f"Downloading with {number_of_threads} threads and {mode}")
-    downloader = Downloader(mode=mode, number_of_th=number_of_threads, timeout=30, max_retry=1, downloading_path='media')
+    config = _get_downloader_config()
+    downloader = Downloader(**config)
     downloader.make_directories()
 
     average_rate = Gauge('Average_Rate', 'Average Rate of download batch of images')
@@ -50,8 +52,8 @@ if __name__ == '__main__':
     file_average_size = Gauge('File_average_size', 'Each file average size')
     file_average_time = Gauge('File_Average_Time', 'Each file spend time')
 
-    for i in range((urls_length//batch_size)+1):
-        if i == urls_length//batch_size:
+    for i in range((urls_length // batch_size) + 1):
+        if i == urls_length // batch_size:
             start_i, end_i = (batch_size * i, urls_length)
         else:
             start_i, end_i = (batch_size * i, batch_size * (i + 1))
